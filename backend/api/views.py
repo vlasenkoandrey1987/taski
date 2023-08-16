4d2e5da1489faa430c87e8cc5a478ba3894b132e
Andrey Vlasenko <vlasenkoandrey1987@gmail.com>
2023-08-16 13:52:29 +0300

    sentry-debug end point added

--- backend/api/views.py
+++ backend/api/views.py
@@ -13,3 +13,7 @@ class TaskView(viewsets.ModelViewSet):
         serializer = self.get_serializer(self.get_object())
         super().destroy(*args, **kwargs)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,6 +8,7 @@ router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
 
 urlpatterns = [
+    path('sentry-debug/', views.trigger_error),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
